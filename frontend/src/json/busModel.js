7f052a86ade683191c13f5bd8201408590839a74
Samuvel Reegan <samuelreegan372@gmail.com>
2025-10-08 06:39:19 +0530

    update busmodel

---
+++ frontend/src/json/busModel.js
@@ -0,0 +1,18 @@
+const busModel = [
+    {
+        id:1,
+        bus_name:"samuvel",
+        start_location:"",
+        end_location:"",
+        bording_point:[],
+        start_time:"",
+        end_time:"",
+        lower_deck_left_sit_sleeper:true,
+        lower_deck_right_sit_sleeper:true,
+        upper_deck_right_sit_sleeper:true,
+        upper_deck_right_sit_sleeper:true,
+        seater_count:31,
+        sleeper_count:36
+
+    }
+]
